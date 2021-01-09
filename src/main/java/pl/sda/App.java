9264c695d5a1f47bd8f7aee09b4381cb7eb7ec3b
Paul <pawelzukowski22@gmail.com>
2021-01-09 16:08:23 +0100

    Commit Lotto/Mini

--- src/main/java/pl/sda/App.java
+++ src/main/java/pl/sda/App.java
@@ -1,13 +1,38 @@
 package pl.sda;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import com.google.gson.Gson;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import pl.sda.model.lotto.Games;
+import pl.sda.model.lotto.Lotto;
+import pl.sda.model.lotto.Mini;
+
+import java.io.IOException;
+
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
-}
+    public static void main( String[] args ) throws IOException, InterruptedException{
+        // http://serwis.mobilotto.pl/mapi_v7/index.php?json=getGames
+
+        Gson gson = new Gson();
+        OkHttpClient client = new OkHttpClient();
+
+            Request request = new Request.Builder()
+                    .url("http://serwis.mobilotto.pl/mapi_v7/index.php?json=getGames")
+                    .build();
+
+             Response response = client.newCall(request).execute();
+
+             String stringresposne = response.body().string();
+             Games dataFromBackend =  gson.fromJson(stringresposne, Games.class);
+
+             System.out.println();
+             System.out.println(dataFromBackend);
+             System.out.println();
+
+             System.out.println(dataFromBackend.getLotto());
+             System.out.println(dataFromBackend.getMini());
+
+            }
+        }
